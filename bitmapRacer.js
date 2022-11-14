8d73865816c10fbebe7747b3aaf23ab0427256ec
Nathan <suprdory@google.com>
2022-11-14 00:47:00 +0000

    bad car mechs

--- bitmapRacer.js
+++ bitmapRacer.js
@@ -52,21 +52,23 @@ class Car {
         // mech + kin
         this.x = x; // x pos
         this.y = y; // y pos
-        this.u = 0; // x vel
-        this.v = 0;  // y vel
+        this.ux = 0; // x vel
+        this.uy = 0;  // y vel
         this.ax = 0; // x accel
         this.ay = 0; // y accel
-        this.m = 10; // mass
+        this.m = 1; // mass
         this.to = 0; //heading torque;
-        this.momI = 100; // moment of inertia
-        this.theta = 0.0; // heading angle
-        this.thetaDot = 0.0;//heading angle deriv
+        this.momI = 10000; // moment of inertia
+        this.theta = 0; // heading angle
+        this.thetaDot = 0.1;//heading angle deriv
 
         // specs
         this.steeringRate = 0.05;
         this.steeringMax = 45 * Math.PI / 180;
         this.torqueRate = 0.5;
         this.torqueMax = 100;
+        this.brakeMax = 1;
+        this.brakeRate = .5;
 
         this.rotMat = calcRotMat(this.theta);
 
@@ -76,20 +78,21 @@ class Car {
         let yr = -this.l / 2 * this.oversize * this.bodyAspect;
         this.coordMat = [[xl, yr], [xl, yf], [0, yf * 1.1], [xr, yf], [xr, yr]];
         this.wheels = [
-            new Wheel(-this.w / 2, this.l / 2, this.wheelWidth, this.wheelAspect, 0.2),
-            new Wheel(this.w / 2, this.l / 2, this.wheelWidth, this.wheelAspect, 0.2),
+            new Wheel(-this.w / 2, this.l / 2, this.wheelWidth, this.wheelAspect, 0.0),
+            new Wheel(this.w / 2, this.l / 2, this.wheelWidth, this.wheelAspect, 0.0),
             new Wheel(-this.w / 2, -this.l / 2, this.wheelWidth, this.wheelAspect, 0.0),
             new Wheel(this.w / 2, -this.l / 2, this.wheelWidth, this.wheelAspect, 0.0)
         ];
     }
     draw(ctx) {
         this.wheels.forEach(wheel => wheel.draw(ctx, this));
-        this.rotMat = calcRotMat(this.theta);
+
         x = this.coordMat;
         x = MatrixProd(x, this.rotMat);
         x = MatrixTrans(x, [this.x, this.y])
+
         ctx.beginPath();
-        ctx.strokeStyle = this.color;
+        ctx.strokeStyle = "white";
         ctx.lineWidth = baseLW;
         // console.table(this.coordMat)
         ctx.moveTo(x[0][0], x[0][1])
@@ -100,6 +103,22 @@ class Car {
         ctx.stroke();
         // console.log(this);
 
+        //velocity vector
+        ctx.beginPath();
+        ctx.strokeStyle = "yellow";
+        ctx.lineWidth = baseLW;
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x + this.ux * vel_scl, this.y + this.uy * vel_scl);
+        ctx.stroke();
+        //acceleration vector
+        ctx.beginPath();
+        ctx.strokeStyle = "blue";
+        ctx.lineWidth = baseLW;
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x + this.ax * acc_scl, this.y + this.ay * acc_scl);
+        ctx.stroke();
+
+
     }
     control(inputState) {
         if (inputState.left) {
@@ -107,8 +126,6 @@ class Car {
             this.wheels[0].rotMat = calcRotMat(this.wheels[0].theta)
             this.wheels[1].theta = Math.min(this.steeringMax, this.wheels[1].theta + this.steeringRate);
             this.wheels[1].rotMat = calcRotMat(this.wheels[1].theta)
-
-
         }
         if (inputState.right) {
             this.wheels[0].theta = Math.max(-this.steeringMax, this.wheels[0].theta - this.steeringRate);
@@ -117,14 +134,26 @@ class Car {
             this.wheels[1].rotMat = calcRotMat(this.wheels[1].theta)
         }
         if (inputState.up) {
+            // front wheel accel
             this.wheels[0].torque = Math.min(this.torqueMax, this.wheels[0].torque + this.torqueRate);
-            // this.wheels[0].rotMat = calcRotMat(this.wheels[0].theta)
             this.wheels[1].torque = Math.min(this.torqueMax, this.wheels[1].torque + this.torqueRate);
-            // this.wheels[1].rotMat = calcRotMat(this.wheels[1].theta)
         }
         else {
             this.wheels[0].torque = 0;
             this.wheels[1].torque = 0;
+        }
+        if (inputState.down || forceBrake) {
+            // 4 wheel braking
+            this.wheels[0].brake = Math.min(this.brakeMax, this.wheels[0].brake + this.brakeRate);
+            this.wheels[1].brake = Math.min(this.brakeMax, this.wheels[1].brake + this.brakeRate);
+            this.wheels[2].brake = Math.min(this.brakeMax, this.wheels[1].brake + this.brakeRate);
+            this.wheels[3].brake = Math.min(this.brakeMax, this.wheels[1].brake + this.brakeRate);
+        }
+        else {
+            this.wheels[0].brake = 0;
+            this.wheels[1].brake = 0;
+            this.wheels[2].brake = 0;
+            this.wheels[3].brake = 0;
         }
 
     }
@@ -133,41 +162,91 @@ class Car {
         this.to = 0;
         this.ax = 0;
         this.ay = 0;
+        let Fx = 0;
+        let Fy = 0;
+
         for (let i = 0; i < 4; i++) {
             let wh = this.wheels[i];
-            // sum acceleration and torque over wheels
-            //accel
-            this.ax = this.ax + wh.torque * Math.sin(this.theta + wh.theta) / this.m;
-            this.ay = this.ay + wh.torque * Math.cos(this.theta + wh.theta) / this.m;
-            // torque
-            this.to = this.to + wh.torque * wh.d*pixPerMetre * Math.sin(wh.theta + wh.phi) / this.momI;
-        }
-        // console.log(this.thetaDot)
-        this.u = this.u + this.ax * dt;
-        this.v = this.v + this.ay * dt;
-        this.x = this.x + this.u * dt;
-        this.y = this.y + this.v * dt;
 
-        this.thetaDot = this.thetaDot + this.to * dt;
+            // velocity
+            //relative to car
+            wh.ux = +this.thetaDot * wh.d * Math.cos(this.theta + wh.phi);
+            wh.uy = -this.thetaDot * wh.d * Math.sin(this.theta + wh.phi);
+            // absolute
+            wh.uxA = wh.ux + this.ux;
+            wh.uyA = wh.uy + this.uy;
+            
+            // parallel/per
+            wh.uApar = -wh.uyA * Math.cos(this.theta + wh.theta) - wh.uxA * Math.sin(this.theta + wh.theta);
+            wh.uAperp = -wh.uyA * Math.sin(this.theta + wh.theta) + wh.uxA * Math.cos(this.theta + wh.theta);
+
+            //thrust
+            wh.Fx = wh.torque * Math.sin(this.theta + wh.theta);
+            wh.Fy = wh.torque * Math.cos(this.theta + wh.theta);
+
+            // braking
+            wh.FBx = Math.sin(this.theta + wh.theta) * wh.uApar * wh.brake;
+            wh.FBy = Math.cos(this.theta + wh.theta) * wh.uApar * wh.brake;
+
+            // lateral friction
+            wh.FLx = -wh.uAperp * Math.cos(this.theta + wh.theta)*mu_lat;
+            wh.FLy = wh.uAperp * Math.sin(this.theta + wh.theta)*mu_lat;
+
+            // torque due to thrust
+            this.to = this.to + wh.torque * wh.d * Math.sin(wh.theta + wh.phi);
+            
+            // torque due to lateral friction
+            this.to = -this.to + wh.FLx * wh.d * Math.sin(wh.theta + wh.phi);
+            this.to = this.to + wh.FLy * wh.d * Math.cos(wh.theta + wh.phi);
+            
+            // resultant wheel force
+            Fx = Fx + wh.Fx + wh.FBx + wh.FLx;
+            Fy = Fx + wh.Fy + wh.FBy + wh.FLy;
+
+        }
+
+        // console.log(this.wheels[0].theta)
+        this.ax = Fx / this.m;
+        this.ay = Fy / this.m;
+        this.ux = this.ux + this.ax * dt;
+        this.uy = this.uy + this.ay * dt;
+        this.x = this.x + this.ux * dt;
+        this.y = this.y + this.uy * dt;
+
+        this.thetaDot = this.thetaDot + this.to/this.momI * dt;
         this.theta = this.theta + this.thetaDot * dt;
+        this.rotMat = calcRotMat(this.theta);
 
     }
 
 }
 class Wheel {
     constructor(x, y, width, aspect, th) {
-        this.x = x;
+        this.x = x; // wheel centre position relative to car
         this.y = y;
+        this.ux = 0; // relative (to car) velocity
+        this.uy = 0;
+        this.uxA = 0; // abs velocity
+        this.uyA = 0;
+        this.Fx = 0;// thrust force components
+        this.Fy = 0;
+        this.FBx = 0;// brake force components
+        this.FBy = 0;
+        this.FLx = 0;// perpendicular force components
+        this.FLy = 0;
+
         this.d = (x ** 2 + y ** 2) ** 0.5;
         this.phi = Math.atan2(x, y);
+        // this.phiy = Math.atan2(y, -x);
         this.width = width;
         this.length = width * aspect;
         this.theta = th;
         this.grip = 1;
         this.color = "white";
         this.torque = 0;
+        this.brake=0;
         let xl = -this.width / 2;
-        let xr = +this.width / 2;
+        let xr = this.width / 2;
         let yf = this.length / 2;
         let yr = -this.length / 2;
         this.coordMat = [[xl, yr], [xl, yf], [xr, yf], [xr, yr]];
@@ -190,6 +269,54 @@ class Wheel {
         }
         ctx.lineTo(x[0][0], x[0][1]);
         ctx.stroke();
+
+        //wheel centre abs coords
+        x = MatrixProd([[this.x, this.y]], car.rotMat);
+        x = MatrixTrans(x, [car.x, car.y])
+
+        //draw rel vel vectors
+        ctx.beginPath();
+        ctx.strokeStyle = "purple";
+        ctx.lineWidth = baseLW;
+        ctx.moveTo(x[0][0], x[0][1]);
+        ctx.lineTo(x[0][0] + this.ux * vel_scl, x[0][1] + this.uy * vel_scl);
+        ctx.stroke();
+
+        //draw abs vel vectors
+        ctx.beginPath();
+        ctx.strokeStyle = "yellow";
+        ctx.lineWidth = baseLW;
+        ctx.moveTo(x[0][0], x[0][1]);
+        ctx.lineTo(x[0][0] + this.uxA * vel_scl, x[0][1] + this.uyA * vel_scl);
+        ctx.stroke();
+
+
+        //draw thrust force vectors
+        ctx.beginPath();
+        ctx.strokeStyle = "green";
+        ctx.lineWidth = baseLW;
+        ctx.moveTo(x[0][0], x[0][1]);
+        ctx.lineTo(x[0][0] + this.Fx * force_scl, x[0][1] + this.Fy * force_scl);
+        ctx.stroke();
+
+
+        //draw brake force vectors
+        ctx.beginPath();
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = baseLW;
+        ctx.moveTo(x[0][0], x[0][1]);
+        ctx.lineTo(x[0][0] + this.FBx * force_scl, x[0][1] + this.FBy * force_scl);
+        ctx.stroke();
+
+        //draw lateral friction force vectors
+        ctx.beginPath();
+        ctx.strokeStyle = "turquoise";
+        ctx.lineWidth = baseLW;
+        ctx.moveTo(x[0][0], x[0][1]);
+        ctx.lineTo(x[0][0] + this.FLx * force_scl, x[0][1] + this.FLy * force_scl);
+        ctx.stroke();
+
+
     }
 
 }
@@ -232,7 +359,7 @@ class InputState {
 }
 
 function anim() {
-    if (n < 1000) {
+    if (n < nMax) {
         requestAnimationFrame(anim);
     }
 
@@ -247,9 +374,16 @@ function anim() {
 
 addEventListener('keydown', (event) => { inputState.set(event) });
 addEventListener('keyup', (event) => { inputState.set(event) });
-const pixPerMetre=0.01
 const dt = 0.1
+const vel_scl = 1;
+const acc_scl = 1;
+const force_scl = 1;
+const mu_lat=1;
+// const forceBrake=false;
+const forceBrake = true;
+
 let n = 0;
+let nMax = 10000;
 let inputState = new InputState;
 let car = new Car(x = 300, y = 400, w = 120, l = 200);
 // car.draw(ctx);
