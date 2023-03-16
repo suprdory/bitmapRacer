1d4a6a38949ff718d49912af1c0c543e1f0c5141
suprdory <n.sparks@imperial.ac.uk>
2023-03-16 12:07:22 +0000

    Update bitmapRacer.js

--- bitmapRacer.js
+++ bitmapRacer.js
@@ -1768,7 +1768,7 @@ class Flash {
         this.y = 15 * pixRat * 2.1
         this.dy = 15 * pixRat;
         this.displayPeriod = 1500;
-        this.message = "Welcome!"
+        this.message = "Happy Birthday Fumi!"
 
         this.mTime = Date.now();
         this.fontFamily = fontFamily;
